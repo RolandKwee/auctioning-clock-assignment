119fbec90b6269544e4a346799818579fe350788
Roland <rkwee@xs4all.nl>
2022-06-19 23:58:18 +0200

    Update Program.cs
    structuring comments added

--- Program.cs
+++ Program.cs
@@ -12,16 +12,18 @@ namespace ACA{
       /// <param name="args"></param>
       public static void Main(string[] args){
         try{
+          // start the console app
           Console.WriteLine("Hello from Auctioning Clock Assignment.");
           DateTime dtStart = DateTime.Now;
+          // read json input file 1
           Ais_Data AD = new Ais_Data();
           string RAD = AD.Parse(@"Data\ais-data.json");
           Console.WriteLine($"Parsed ais-data.json: {RAD}");
-          //
+          // read json input file 2
           Characteristics CH = new Characteristics();
           string Rch = CH.Parse(@"Data\characteristics.json");
           Console.WriteLine($"Parsed characteristics.json: {Rch}"); 
-          //
+          // conclusion of the console app
           int ms = (int)(0.5 + (DateTime.Now - dtStart).TotalMilliseconds);
           Console.WriteLine($"Program finished in {ms} ms.");
         }
